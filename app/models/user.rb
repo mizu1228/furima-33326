00b042f98278d102bb2f2850536dcb16795a2b4e
mizu1228 <m.1228.jcm900@gmail.com>
2021-01-30 07:52:39 +0900

    userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, :email, :encrypted_password, :first_name,
+            :last_name, :first_name_kana, :last_name_kana, :birth_day, presence: true
 end
