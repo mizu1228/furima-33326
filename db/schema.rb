dfe9eb327bfc7a9008e9c43c6436252306bc48f6
mizu1228 <m.1228.jcm900@gmail.com>
2021-01-30 18:26:47 +0900

    ミスで消したitemモデルの再成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_071936) do
+ActiveRecord::Schema.define(version: 2021_01_30_092228) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
