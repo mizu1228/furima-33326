148f56e7917eb28a0a53b0b59bdf539c6c61bb81
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-01 09:17:38 +0900

    都道府県、カテゴリーモデルの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   validates :item, :explanation, :price , presence: true
+  validates :prefecture_id, :category_id, numericality: { other_than: 0 }
 
 belongs_to :user
+belongs_to :prefecture
+belongs_to :category
 
 end
