d4f1ab407e968d0410a7a87c15223541dfc1409a
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-01 10:53:07 +0900

    軽微な修正、アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   validates :item, :explanation, :price , presence: true
-  validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id numericality: { other_than: 0 }
+  validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id, numericality: { other_than: 0 }
 
 belongs_to :user
 belongs_to :prefecture, :category, :when_post,:item_status, :shipping
+has_one_attached :image
 
 end
