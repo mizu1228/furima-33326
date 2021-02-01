fc25c37b2c3525e52420fb6d2390702e525b67a4
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-01 17:31:02 +0900

    エラーの修正、可読性の向上
    - [item_params]を(item_params)に修正
    - ユーザーが紐づいていなかったので紐づけた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -8,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new[item_params]
+    @item = Item.new(item_params)
       if @item.save
         redirect_to root_path
       else
@@ -21,9 +23,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(
-      :item, :explanation, :price, :prefecture, :category,
-      :when_post,:item_status, :shipping, :image
-    )
+      :item, :explanation, :price, :prefecture_id,
+      :category_id, :when_post_id,:item_status_id, :shipping_id, :image
+    ).merge(user_id: current_user.id)
   end
 
 end
