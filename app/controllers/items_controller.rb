af8d86a0d1c029a5e331a2c7812f40840d186313
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-01 10:54:14 +0900

    createアクションの設定、中身の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,26 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new[item_params]
+      if @item.save
+        redirect_to root_path
+      else
+        render :new
+      end
   end
   
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :item, :explanation, :price, :prefecture, :category,
+      :when_post,:item_status, :shipping, :image
+    )
+  end
+
 end
