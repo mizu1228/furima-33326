0ef07aa42c7a0e7cb6901ae7e02da659839c64c5
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-01 17:31:44 +0900

    金額範囲の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :item, :explanation, :price , presence: true
+  validates :item, :explanation, presence: true
   validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id, numericality: { other_than: 0 }
+  validates :price, presence: true, numericality: {only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "300~9999999の間で入力してください" }
+  
 
 belongs_to :user
-belongs_to :prefecture, :category, :when_post,:item_status, :shipping
+belongs_to :prefecture, :category, :when_post, :item_status, :shipping
 has_one_attached :image
 
 end
