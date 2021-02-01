5a1f5586e40e7e2bf6b4b3fff2a6fab7e899f32d
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-01 17:26:43 +0900

    itemをtext型にした

--- db/migrate/20210130092228_create_items.rb
+++ db/migrate/20210130092228_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
-      t.string     :item,           null: false
+      t.text     :item,           null: false
       t.text       :explanation,    null: false
       t.integer    :price,          null: false
       t.references :user,           null: false, foreign_key: true
