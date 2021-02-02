ae613c56d7bfc8d7fc21bccad2ebb9983085c2fc
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-02 20:16:20 +0900

    商品名と説明に関するバリデーションをモデルに設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :item, :explanation, presence: true
+  validates :item, presence: true, length: { maximum: 40}
+  validates :explanation, presence: true, length: { maximum: 1000 }
   validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id, numericality: { other_than: 0 }
   validates :price, presence: true, numericality: {only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は300~9999999の間で入力してください" }
   
