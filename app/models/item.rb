e0a7e6d8bb6c000490a02aca357b77ef58f75346
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-02 19:49:59 +0900

    メッセージの微修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
 
   validates :item, :explanation, presence: true
   validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id, numericality: { other_than: 0 }
-  validates :price, presence: true, numericality: {only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "300~9999999の間で入力してください" }
+  validates :price, presence: true, numericality: {only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は300~9999999の間で入力してください" }
   
 
 belongs_to :user
