5d3ec5c080b877d23d42ac98a9970b7a44dd6e41
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-03 11:49:46 +0900

    presenceのバリデーションをwith_optionでまとめた

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :image, presence: true
-  validates :item, presence: true, length: { maximum: 40}
-  validates :explanation, presence: true, length: { maximum: 1000 }
-  validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id, numericality: { other_than: 0 }
-  validates :price, presence: true, numericality: {only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は半角数字300~9999999の間で入力してください" }
-  
+  belongs_to :user
+  belongs_to :prefecture, :category, :when_post, :item_status, :shipping
+  has_one_attached :image
 
-belongs_to :user
-belongs_to :prefecture, :category, :when_post, :item_status, :shipping
-has_one_attached :image
+  with_options presence: true do
+    validates :image
+    validates :item, length: { maximum: 40}
+    validates :explanation, length: { maximum: 1000 }
+    validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id, numericality: { other_than: 0 }
+    validates :price, numericality: {only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は半角数字300~9999999の間で入力してください" }
+  end
 
 end
