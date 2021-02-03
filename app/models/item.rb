6d9eabac52f8780645b2532f6aa5c1d36582b104
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-03 21:38:31 +0900

    可読性の向上

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
     validates :item, length: { maximum: 40}
     validates :explanation, length: { maximum: 1000 }
     validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id, numericality: { other_than: 0 }
-    validates :price, numericality: {only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は半角数字300~9999999の間で入力してください" }
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は半角数字300~9999999の間で入力してください" }
   end
 
 end
