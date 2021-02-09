6138674b1a68ec0f2af423e75ca7649286103c73
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-10 08:14:39 +0900

    Formオブジェクトを編集

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,6 +1,10 @@
 class Shopping
   include ActiveModel::Model
-  attr_accessor :order, :ship_address
+  attr_accessor :building, :address, :city, :prefecture_id, :zip_code, :phone_num, :user_id, :item_id
 
   # バリデーションと各テーブルに保存する処理を書く
+
+  with_options presence: true do
+    validates :address, :city, :prefecuture_id, :zip_code, :phone_num
+  end
 end
