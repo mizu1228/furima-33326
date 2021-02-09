7b4facb03eec8270f0ab64346909f88adac08631
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-09 10:44:56 +0900

    同じコードをメソッドにまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  
+  before_action set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order("Created_at DESC")
@@ -19,25 +19,21 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
     end
   end
-  
 
   private
 
@@ -53,6 +49,10 @@ class ItemsController < ApplicationController
       :shipping_id, 
       :image
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
