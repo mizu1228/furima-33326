24aa9a6f4b642e687832438a5a29c1e25511a78a
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-10 09:56:06 +0900

    Formオブジェクトにバリデーションを記載

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,10 +1,11 @@
 class Shopping
   include ActiveModel::Model
-  attr_accessor :building, :address, :city, :prefecture_id, :zip_code, :phone_num, :user_id, :item_id
-
-  # バリデーションと各テーブルに保存する処理を書く
+  attr_accessor :building, :address, :city, :prefecture_id, :zip_code, :phone_num, :user_id, :item_id, :order_id
 
   with_options presence: true do
-    validates :address, :city, :prefecuture_id, :zip_code, :phone_num
+    validates :address, :city, message: "市区町村、番地は必須です"
+    validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフン(-)を入れてください" }
+    validates :phone_num, format: { with: /\A\d{10,11}\z/, message: "は半角数字10または11桁で入力してください" }
+    validates :prefecture_id, numericality: { other_than: 0, message: "都道府県は必須です" }
   end
 end
