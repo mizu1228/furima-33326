cce997275ab4cb59a6e95a3be220c0cac741c46b
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-11 15:42:23 +0900

    [:id]を[:item_id]に修正した

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,13 @@
 class OrdersController < ApplicationController
 
   def index
-    @shopping = Item.new
-    @item = Item.find(8)  #数字を入れたらうまくいったので、IDを取得できるようにしたい
+    @shopping = Shopping.new
+    @item = Item.find(params[:item_id])  
   end
 
   def create
-    @shopping = shopping.find(params[:id])
+    @item = Item.find(params[:item_id])
+    @shopping = Shopping.new(order_params)
       if @shopping.valid?
         @shopping.save
         redirect_to root_path
@@ -21,18 +22,5 @@ class OrdersController < ApplicationController
     params.require(:shopping).permit(:building, :address, :city, :prefecture_id, :zip_code, :phone_num, :item_id, :user_id)
   end
 
-  def item_params
-    params.require(:item).permit(
-      :item, 
-      :explanation, 
-      :price, 
-      :prefecture_id,
-      :category_id, 
-      :when_post_id, 
-      :item_status_id, 
-      :shipping_id, 
-      :image
-    ).merge(user_id: current_user.id)
-  end
 
 end
