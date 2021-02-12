e8d471e222e20ee30dacb403166f459a83e35ced
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-12 16:37:51 +0900

    購入ボタンをクリックすると、イベントが発火するようにした

---
+++ app/javascript/card.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    const pay = document.getElementById("pay-btn")
+
+    pay.addEventListener('click', function(){
+      console.log("クリック！")
+    })
+  })
