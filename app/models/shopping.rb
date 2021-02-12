ebdaa371e69b4519bd4f9a4244290939e98ef810
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-12 14:22:20 +0900

    Formオブジェクトを編集
    ・不要なカラムの記述を削除
    ・各モデルに、必要なデータを保存できるように修正

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,19 +1,18 @@
 class Shopping
 
   include ActiveModel::Model
-  attr_accessor :item, :price, :image, :building, :address, :city, :prefecture_id, :zip_code, :phone_num, :user_id, :item_id, :order_id
+  attr_accessor :building, :address, :city, :prefecture_id, :zip_code, :phone_num, :user_id, :item_id
 
   with_options presence: true do
     validates :address, :city
     validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフン(-)を入れてください" }
     validates :phone_num, format: { with: /\A\d{10,11}\z/, message: "は半角数字10または11桁で入力してください" }
-    validates :prefecture_id, numericality: { other_than: 0, message: "は必須です" }
   end
+  validates :prefecture_id, numericality: { other_than: 0, message: "は必須です" }
 
   def save
-    item = Item.create(item: item, price: price, image: image)
-    ShipAddress.create(building: building, address: address, city: city, prefecture_id: prefecture_id, zip_code: zip_code, phone_num: phone_num, user_id: user_id, item_id: item_id)
-    Order.create(user_id: user_id, item_id: item_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShipAddress.create(building: building, address: address, city: city, prefecture_id: prefecture_id, zip_code: zip_code, phone_num: phone_num, order_id: order.id)
   end
 
 end
