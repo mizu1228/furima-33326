fc77ba331b9742a144e47edd94cbabff379bf690
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-13 08:01:18 +0900

    カード情報を削除した。送信処理を追加した。

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -23,6 +23,13 @@ function pay() {
           const tokenObj = `<input value=${token} name='token' type='hidden'>`;
           renderDom.insertAdjacentHTML("beforeend", tokenObj);
         }
+
+          document.getElementById("card-number").removeAttribute("name");
+          document.getElementById("card-exp-month").removeAttribute("name");
+          document.getElementById("card-exp-year").removeAttribute("name");
+          document.getElementById("card-cvc").removeAttribute("name");
+
+          document.getElementById("charge-form").submit();
       });
     });
   };
