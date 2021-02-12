fef6d9c9e7099cc149fbf008350753f8b45dd8ff
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-12 21:32:34 +0900

    トークンをconsole.logに表示するまでは出来た

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,10 +1,28 @@
 function pay() {
-    const pay = document.getElementById("charge-form")
+    Payjp.setPublicKey("pk_test_34cf9ddce0c88cc64629448b");
+
+    const pay = document.getElementById("charge-form");
 
     pay.addEventListener('submit', function(e) {
       e.preventDefault(); 
-      console.log("クリック！")
-    })
-  }
+      
+      const formResult = document.getElementById("charge-form");
+      const formData = new FormData(formResult);
+
+      const card = {
+        number:    formData.get("shopping[number]"),
+        exp_month: formData.get("shopping[exp_month]"),
+        exp_year:  `20${formData.get("shopping[exp_year]")}`,
+        cvc:       formData.get("shopping[cvc]")
+      };
+
+      Payjp.createToken(card, (status, response) => {
+        if (status == 200) {
+          const token = response.id;
+          console.log(token)
+        }
+      });
+    });
+  };
 
   window.addEventListener('load', pay );
