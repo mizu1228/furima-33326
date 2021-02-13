e1afaf223b1410bc4f94382d10514ee26cc3c02d
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-13 11:18:00 +0900

    エラーメッセージの変更

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -5,8 +5,8 @@ class Shopping
 
   with_options presence: true do
     validates :address, :city, :token
-    validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフン(-)を入れてください" }
-    validates :phone_num, format: { with: /\A\d{10,11}\z/, message: "は半角数字10または11桁で入力してください" }
+    validates :zip_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフン(-)を入れて、正しい桁数で入力してください" }
+    validates :phone_num, format: { with: /\A\d{10,11}\z/, message: "は半角数字10または11桁で入力してください(ハイフン不要)" }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "は必須です" }
 
