74828d5c0b84ac34ccc131661e448168d845802a
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-14 07:24:58 +0900

    未ログインユーザは購入ページに行けない

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
 before_action :set_item, only: [:index, :create]
+before_action :authenticate_user!, only: [:index, :create]
+
   def index
     if current_user.id != @item.user_id && @item.order.nil?
     @shopping = Shopping.new
