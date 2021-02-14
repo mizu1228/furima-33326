fa81f74a72d550b767c2a12cb457c85548eca7f1
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-14 18:30:47 +0900

    FactoryBotの中身をわかりやすくした

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name                   {Faker::Name.initials(number: 2)}
+    name                   { 'testねーむ' }
     email                  {Faker::Internet.free_email}
-    password               {"testpass123"}
+    password               { "testpass123" }
     password_confirmation  {password}
-    first_name             {"テストみょう字"}
-    last_name              {"てすト名前"}
-    first_name_kana        {"ミョウジ"}
-    last_name_kana         {"ナマエ"}
+    first_name             { "テストみょう字" }
+    last_name              { "てすト名前" }
+    first_name_kana        { "ミョウジ" }
+    last_name_kana         { "ナマエ" }
     birth_day              {Faker::Date.birthday(min_age: 0, max_age: 85)}
   end
 end
