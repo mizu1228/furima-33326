160b8a92583e6e9febe4c4825fb6cb72b0d8d42f
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-14 13:36:18 +0900

    FactoryBotの値を追加

--- spec/factories/shoppings.rb
+++ spec/factories/shoppings.rb
@@ -5,6 +5,9 @@ FactoryBot.define do
     prefecture_id  { 2 }
     city           { '八戸市' }
     address        { '大字市川町字桔梗野' }
+    building       { '過去いたとこ！' }
     phone_num      { '99999999999' }
+    user_id        { 1 }
+    item_id        { 1 }
   end
 end
