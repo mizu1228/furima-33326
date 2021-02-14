9987ba8573a679a6740315424af6d1916c3d8d8f
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-14 10:54:22 +0900

    FactoryBotの中身を編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name                   {Faker::Name.initials(number: 2)}
     email                  {Faker::Internet.free_email}
-    password               {Faker::Internet.password(min_length: 6)}
+    password               {"testpass123"}
     password_confirmation  {password}
     first_name             {"テストみょう字"}
     last_name              {"てすト名前"}
