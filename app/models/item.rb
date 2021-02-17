4ed04df8373a8a9c6c82ad5e85602eac7a9662e8
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-17 13:50:23 +0900

    エラーメッセージを変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
     validates :image
     validates :item, length: { maximum: 40}
     validates :explanation, length: { maximum: 1000 }
-    validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id, numericality: { other_than: 0 }
+    validates :prefecture_id, :category_id, :when_post_id, :shipping_id, :item_status_id, numericality: { other_than: 0, message: "は選択必須です" }
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は半角数字300~9999999の間で入力してください" }
   end
 
