22c4e7666695d6e4a1e0a604bb7a42f0f9c892cb
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-18 13:47:53 +0900

    パスワードなしで編集ができるようにしたが、問題あり
    パスワードの正規表現エラーにかかってしまうので、今は外している
    エラーが起きずかつ登録時の正規表現も作動するように設定する

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,29 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :name, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day 
+    validates :name, :email, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day 
     validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
     validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください' }
-
   end
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には記号以外、半角英数字の両方を含めて設定してください'
+
+  # PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  # validates_format_of :password, with: PASSWORD_REGEX, message: 'には記号以外、半角英数字の両方を含めて設定してください'
+
+  def update_without_current_password(params, *options)
+    params.delete(:password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 
   has_many :items
   has_many :orders
+  has_one_attached :image
 end
 
