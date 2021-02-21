14341aa37b95280ca0e79ee904ca4aadffc0c226
mizu1228 <m.1228.jcm900@gmail.com>
2021-02-21 20:24:05 +0900

    画像のプレビュー機能（まずは１枚）

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,16 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+   const ImgPreview = document.getElementById('img-preview');
+   document.getElementById('item-image').addEventListener('change', function(e){
+     const file = e.target.files[0];
+     const blob = window.URL.createObjectURL(file);
+
+     const imageElement = document.createElement('div');
+     const blobImage = document.createElement('img');
+     blobImage.setAttribute('src', blob);
+
+     imageElement.appendChild(blobImage);
+     ImgPreview.appendChild(imageElement);
+    });
+  });
+}
